5d6362f27a8e3ec2d4cc99b4e7c42f1ccb27f610
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2023-12-20 17:03:35 +0530

    added cards

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,19 +42,19 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Full Stack Development",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Data Science",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Algorithm Design",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Mathematical Economics",
       icon: creator,
     },
   ];
